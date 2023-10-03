2e58ed9f36fd99954e2a1181d4ecb03b99b46684
Walter N <2760walter@gmail.com>
2023-10-03 14:17:24 +0300

    render beer review

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ function fetchElement(){
     const beerName = document.getElementById('beer-name');
     const beerImage = document.getElementById('beer-image');
     const beerDescription = document.getElementById('beer-description');
-
-
+    let beerReviews = document.getElementById('review-list');
+    
 
     return fetch('http://localhost:3000/beers/1')
     .then(res => res.json())
@@ -22,7 +22,27 @@ function fetchElement(){
         beerName.innerText = data.name;
         beerImage.src = data.image_url;
         beerDescription.innerText = data.description;
+        beerReviews.innerHTML="";
+        data.reviews.forEach(
+            review =>{
+                const listItem = document.createElement('li');
+                listItem.textContent=review;
+                beerReviews.appendChild(listItem);          
+        });
+        console.log(data.reviews);
+        // data.reviews.map(x=>{
+        //     listItem.innerText=x;
+        //     beerReviews.appendChild(listItem);
+        // });        
+
     });
+
+    function addReview(list){
+        for(const item of list){
+            listItem.innerText = item;
+            return listItem;
+        }
+    }
 }
 
 fetchElement();
