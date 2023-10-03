cded047c1791bdeed7ac27a3eef7b1419a7bf668
Walter N <2760walter@gmail.com>
2023-10-03 11:55:57 +0300

    get beer name, img and description on page load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 // Code here
+
+//define the beerdetails region
+
+const beerDetails = document.querySelector('div.beer-details');
+
+const init = () =>{
+    
+}
+
+function fetchElement(){
+
+    const beerName = document.getElementById('beer-name');
+    const beerImage = document.getElementById('beer-image');
+    const beerDescription = document.getElementById('beer-description');
+
+
+
+    return fetch('http://localhost:3000/beers/1')
+    .then(res => res.json())
+    .then(data =>{
+        beerName.innerText = data.name;
+        beerImage.src = data.image_url;
+        beerDescription.innerText = data.description;
+    });
+}
+
+fetchElement();
+
+
