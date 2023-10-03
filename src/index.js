0ece2c4d9693f19de6f6b7e7ce2193bc2cf21794
Walter N <2760walter@gmail.com>
2023-10-03 15:20:31 +0300

    add beer Nav list

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@
 //define the beerdetails region
 
 const beerDetails = document.querySelector('div.beer-details');
+const beerNav  = document.getElementById('beer-list');
+const beerName = document.getElementById('beer-name');
+
 
 const init = () =>{
     
 }
 
-function fetchElement(){
+function fetchElement(id){
 
-    const beerName = document.getElementById('beer-name');
+    
     const beerImage = document.getElementById('beer-image');
     const beerDescription = document.getElementById('beer-description');
     let beerReviews = document.getElementById('review-list');
     
 
-    return fetch('http://localhost:3000/beers/1')
+    return fetch(`http://localhost:3000/beers/${id}`)
     .then(res => res.json())
     .then(data =>{
         beerName.innerText = data.name;
@@ -29,22 +32,43 @@ function fetchElement(){
                 listItem.textContent=review;
                 beerReviews.appendChild(listItem);          
         });
-        console.log(data.reviews);
-        // data.reviews.map(x=>{
-        //     listItem.innerText=x;
-        //     beerReviews.appendChild(listItem);
-        // });        
+              
 
     });
 
-    function addReview(list){
-        for(const item of list){
-            listItem.innerText = item;
-            return listItem;
-        }
-    }
+    // function addReview(list){
+    //     for(const item of list){
+    //         listItem.innerText = item;
+    //         return listItem;
+    //     }
+    // }
 }
 
-fetchElement();
+function setBeerNav(){
+    //get list of beers
+    //get the names of each bear
+    //create a list and append to the ul
+
+    fetch(`http://localhost:3000/beers`).then(res => res.json())
+    .then(data =>{
+    
+
+    beerNav.innerHTML=""; //clear the Nav list        
+    for(let obj in data){
+        const listItem = document.createElement('li');      
+        
+        listItem.textContent=data[obj].name;
+        beerNav.appendChild(listItem);  
+
+
+    }    
+    });
+    
+}
+
+fetchElement(1);
+setBeerNav()
+
+
 
 
